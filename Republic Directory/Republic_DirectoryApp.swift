da72de3af2f1af139193dd2482afc660256c3dd3
Patrick Mayo <patrickmayo@churchofjesuschrist.org>
2022-06-23 15:06:19 -0600

    Added CoreData to app

--- Republic Directory/Republic_DirectoryApp.swift
+++ Republic Directory/Republic_DirectoryApp.swift
@@ -9,9 +9,17 @@ import SwiftUI
 
 @main
 struct Republic_DirectoryApp: App {
+    @Environment(\.scenePhase) var scenePhase
+    let persistenceController = PersistenceController.shared
+    
     var body: some Scene {
         WindowGroup {
             SplashScreenView()
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+        .onChange(of: scenePhase) { _ in
+            persistenceController.save()
         }
     }
 }
